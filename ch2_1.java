bde01586f3504ee48c12f1084f74803c44cac16e
409737227 <116100723+409737227@users.noreply.github.com>
2024-07-02 11:56:41 +0800

    Add files via upload

---
+++ ch2_1.java
@@ -0,0 +1,8 @@
+public class ch2_1 {
+    public static void main(String[]args){
+        int num1=2;
+        int num2=3;
+        System.out.println(num1+"cat are running");
+        System.out.println(num2+"dog are running");
+    }
+}
