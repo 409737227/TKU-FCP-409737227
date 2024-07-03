c0db199f506ee3d830a76cdb28bd775e6cf5f87c
409737227 <116100723+409737227@users.noreply.github.com>
2024-07-03 13:55:52 +0800

    Add files via upload

---
+++ hm_01.java
@@ -0,0 +1,19 @@
+public class hm_01 {
+    public static void main(String[] args) {
+        // 宣告並初始化整數變數age，值為25
+        int age = 25;
+        
+        // 宣告並初始化字元變數initial，值為'A'
+        char initial = 'A';
+        
+        // 宣告並初始化字串變數name，值為"John"
+        String name = "John";
+        
+        // 打印每個變數的值
+        System.out.println("age: " + age);
+        System.out.println("initial: " + initial);
+        System.out.println("name: " + name);
+    }
+    
+    
+}
---
+++ hm_02.java
@@ -0,0 +1,8 @@
+public class hm_02 {
+    public static void main(String[] args) {
+        // 輸出個人信息
+        System.out.println("名字: 楊佳霓");
+        System.out.println("年齡: 24");
+        System.out.println("興趣愛好: 插畫, 料理, 唱歌, 出遊");
+    }
+}
