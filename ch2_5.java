05c91a3ce3165523155a33aee35ffb1011c0ec0d
409737227 <116100723+409737227@users.noreply.github.com>
2024-07-03 11:42:39 +0800

    Add files via upload

---
+++ ch2_5.java
@@ -0,0 +1,10 @@
+public class ch2_5 {
+    public static void main(String[]args){
+        int a=2;
+        int b=3;
+        int c=a+b;//先新增一個變數把a跟b加總
+
+        System.out.println("a+b="+c);
+    }
+}
+
